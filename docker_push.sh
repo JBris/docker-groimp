69cecdf02ef0eea4ecf4f5108da4277f01d3fc5a
Junqi_Zhu <junqi108@gmail.com>
2022-11-14 16:32:13 +1300

    add command to automatically retrieve hash

--- docker_push.sh
+++ docker_push.sh
@@ -9,5 +9,7 @@
 echo $CR_PAT | sudo docker login ghcr.io -u USERNAME --password-stdin
 
 # To find DOCKER_IMAGE_HASH: docker image ls
-docker tag "$DOCKER_IMAGE_HASH" "ghcr.io/${CONTAINER_REG_USER}/${CONTAINER_REG_NAME}:latest"
+IFS='\n' read -r -a array <<< "$(sudo docker images --filter=reference='debian:stable*' --format '{{.ID}}')"
+echo "${array[0]}"
+docker tag "${array[0]}" "ghcr.io/${CONTAINER_REG_USER}/${CONTAINER_REG_NAME}:latest"
 docker push "ghcr.io/${CONTAINER_REG_USER}/${CONTAINER_REG_NAME}:latest"
